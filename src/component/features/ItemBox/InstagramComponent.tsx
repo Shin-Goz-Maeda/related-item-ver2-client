aa3c2181f97b007376093aa7a9eb4afe10f88cff
Shin-Goz-Maeda <km.tks.mml710@gmail.com>
2023-06-01 11:44:48 +0900

    git 問題解消のためブランチは違うがプッシュした。

--- src/component/features/ItemBox/InstagramComponent.tsx
+++ src/component/features/ItemBox/InstagramComponent.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react';
-import NothingItemComponent from '../nothingItemComponent';
+import NothingItemComponent from '../NothingItemComponent';
 
 type Props = {
   instagramData: any;
