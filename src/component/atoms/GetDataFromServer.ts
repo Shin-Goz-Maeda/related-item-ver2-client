acee75c2a6fffbbb7896bc059c0c30b60f151ceb
Shin-Goz-Maeda <km.tks.mml710@gmail.com>
2023-06-09 00:19:38 +0900

    定数を変更

--- src/component/atoms/GetDataFromServer.ts
+++ src/component/atoms/GetDataFromServer.ts
@@ -1,14 +1,14 @@
-import { HOST_DOMAIN } from '../../constants/index';
-import type { JSONType } from '../../types/json';
+import { SERVER_DOMAIN } from "../../constants/index";
+import type { JSONType } from "../../types/json";
 
 export const getDataFromServer = (
   path: string,
   someSetFunc: React.Dispatch<React.SetStateAction<JSONType | undefined>>,
   setLoading: React.Dispatch<React.SetStateAction<boolean>>
 ) => {
-  if (HOST_DOMAIN !== undefined) {
+  if (SERVER_DOMAIN !== undefined) {
     const getData = () => setLoading(false);
-    fetch(HOST_DOMAIN + path)
+    fetch(SERVER_DOMAIN + path)
       .then((res) => {
         res.json().then((data) => {
           someSetFunc(data);
