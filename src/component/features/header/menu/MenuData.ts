6bd6d01beadc00ac62dd2f883f7f870a83ba1209
Shin-Goz-Maeda <km.tks.mml710@gmail.com>
2023-06-14 23:25:39 +0900

    ヘッダー内のメニューを作成

---
+++ src/component/features/header/menu/MenuData.ts
@@ -0,0 +1,15 @@
+// メニュー情報
+export const MenuData = [
+  {
+    title: "アカウント情報",
+    link: "/accountinfoPage",
+  },
+  {
+    title: "パスワードリセット",
+    link: "/pwResetPage",
+  },
+  {
+    title: "退会",
+    link: "/withdrawalConfirmationPage",
+  },
+];
